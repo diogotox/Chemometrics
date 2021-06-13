681f2b3380864e9f02f265c29f922bf2a88868e0
Diogo Costa <91.diogo@gmail.com>
2021-06-13 19:35:11 +0100

    Inicial PCA analysis

--- Scripts/PMR_predction.R
+++ Scripts/PMR_predction.R
@@ -3,21 +3,31 @@ rm(list=ls())
 
 #loading packages
 #library("tidyverse")
-library("dplyr")
-
+#library("dplyr")
+#library("factoextra")
 #Loading and cleaning dataset
 
 descriptors = as.data.frame(read.csv("Data/descriptors.csv", header = T))
 View(descriptors)
 
+# Removing non numerical variables
 descriptortidy = descriptors %>%
   select(-c(1:3,22))
 
-View(descriptortidy)
 
 # Principal Component Analysis PCA
 
-pca = pc
+pca = prcomp(descriptortidy, center = T, scale. = T )
+summary(pca)
+
+#
+
+biplot(pca)
+
+# Beautiful visualization with factoextra packaged
+
+fviz_eig(pca)
 
 
-?prcomp
+
+
